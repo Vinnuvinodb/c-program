5a94a1b649d016a7a958bee1ee20da4ca17656de
Vinnuvinodb <122351754+Vinnuvinodb@users.noreply.github.com>
2023-01-27 23:24:19 -0800

    Create 39.c

---
+++ 39.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void swap(int *x, int *y) {
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+int main() {
+    int x, y;
+    printf("Enter the first number: ");
+    scanf("%d", &x);
+    printf("Enter the second number: ");
+    scanf("%d", &y);
+
+    printf("Before swapping: x = %d, y = %d\n", x, y);
+    swap(&x, &y);
+    printf("After swapping: x = %d, y = %d\n", x, y);
+
+    return 0;
+}
