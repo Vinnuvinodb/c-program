2a8d1564470ec97a40b26284077d1a246791270c
Vinnuvinodb <122351754+Vinnuvinodb@users.noreply.github.com>
2023-02-19 22:55:24 -0800

    Create 48.c

---
+++ 48.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+int n;
+printf("Enter a number");
+scanf("%d",&n); 
+printf("\nSquare of the number %d is %d",n,n*n);
+printf("\nCube of the number %d is %d",n,n*n*n);
+return 0;
+}
